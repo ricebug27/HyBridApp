fa8e1f2fb1cd6e90e9ef991bed9de30bcaefdb2f
william <william@williamtekiMacBook.local>
2014-11-30 22:51:09 +0800

    Initial Commit

---
+++ HybridApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HybridApp
+//
+//  Created by william on 14/11/30.
+//  Copyright (c) 2014年 ricebug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
